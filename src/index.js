c0af6b0fa20b9999c85f8db63260aa6924e789ee
wojtekojtek <49075395+wojtekojtek@users.noreply.github.com>
2025-11-05 13:09:34 +0000

    random

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 console.log("weLCOME")
 alert("chyba tak")
+
+const pages = [
+  '3eafeff2-a434-4c5f-aaf1-ca7d37769194.html',
+  '908c4441-196c-4368-8830-b9f8584ba410.html',
+  '8915c676-01c8-47ed-8881-fcf6a259eaa1.html',
+];
+
+function goToRandomPage() {
+  const randomPage = pages[Math.floor(Math.random() * pages.length)];
+  window.location.href = 'random/' + randomPage;
+}
