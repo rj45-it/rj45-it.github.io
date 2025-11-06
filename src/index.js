8d880ecada8b474969e94e1b2bd5e876b6b9a27b
wojtekojtek <49075395+wojtekojtek@users.noreply.github.com>
2025-11-06 07:16:19 +0000

    pick random page button

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const pages = [
   '3eafeff2-a434-4c5f-aaf1-ca7d37769194.html',
   '908c4441-196c-4368-8830-b9f8584ba410.html',
   '8915c676-01c8-47ed-8881-fcf6a259eaa1.html',
+  '2ddbfba0-3449-46e9-aa76-87571e6afba3.html',
 ];
 
 function goToRandomPage() {
