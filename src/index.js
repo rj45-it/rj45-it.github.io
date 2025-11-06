47cd232f2d63edba7adbb8d1d0e3e7b3390aff81
wojtekojtek <49075395+wojtekojtek@users.noreply.github.com>
2025-11-06 07:54:03 +0000

    Poprawa nawigacji do stron losowych oraz aktualizacja linków w HTML

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ const pages = [
   '908c4441-196c-4368-8830-b9f8584ba410.html',
   '8915c676-01c8-47ed-8881-fcf6a259eaa1.html',
   '2ddbfba0-3449-46e9-aa76-87571e6afba3.html',
+  'https://youtu.be/xvFZjo5PgG0?feature=shared',
+  'https://www.youtube.com/watch?v=D_h2G6QMMjA',
+  'https://gógl.pl',
 ];
 
 function goToRandomPage() {
   const randomPage = pages[Math.floor(Math.random() * pages.length)];
-  window.location.href = 'random/' + randomPage;
+  if (randomPage.startsWith('http://') || randomPage.startsWith('https://')) {
+    window.location.href = randomPage;
+  } else {
+    window.location.href = 'random/' + randomPage;
+  }
 }
